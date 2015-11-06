a4f72b4c22a1128846e093c917d405f291dbeba4
Paul Bienkowski <pb@opatut.de>
2015-11-06 22:19:38 +0100

    Add index route and fix config

--- server/anchorite/api/routes.py
+++ server/anchorite/api/routes.py
@@ -1,1 +1,10 @@
-# This is for Caro <3
+from anchorite import app
+
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# This file is for Caro <3
