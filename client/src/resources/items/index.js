44d4f8253060b0a98b5a26b3fd643005d5b3a35e
Paul Bienkowski <pb@opatut.de>
2015-11-07 19:43:30 +0100

    Icons!

--- client/src/resources/items/index.js
+++ client/src/resources/items/index.js
@@ -1,3 +1,5 @@
 export branch from './branch.png';
 export pine_cone from './pine_cone.png';
 export sickener from './sickener.png';
+export grass from './grass.png';
+export wild_strawberry from './wild_strawberry.png';
