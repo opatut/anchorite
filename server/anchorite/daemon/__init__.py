e47194a79276a3a58647d6e4f02e5a62e0d3e1e9
Paul Bienkowski <pb@opatut.de>
2015-11-08 15:52:10 +0100

    Autoqueue

--- server/anchorite/daemon/__init__.py
+++ server/anchorite/daemon/__init__.py
@@ -1,6 +1,6 @@
 import time, random
 from anchorite import manager, db
-from anchorite.common.models import User, Action, GameState
+from anchorite.common.models import User, Action, GameState, CollectAction
 
 @manager.command
 def daemon():
@@ -9,10 +9,15 @@ def daemon():
     print(game_state.tick)
     while True:
         for action in Action.query.filter(Action.end <= game_state.tick).all():
+            user = action.user
             action.execute()
             db.session.delete(action)
 
         #time.sleep(random.uniform(0, 1))
+        for user in User.query.all():
+            if user.actions.count() == 0:
+                user.queue_action(CollectAction(), 5)
+
 
         tick = (time.time() - start_tick)
         game_state.tick = tick
