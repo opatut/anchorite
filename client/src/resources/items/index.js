9093f4848f8a29789d91eaed207f5dc4ccb2a69b
Paul Bienkowski <pb@opatut.de>
2015-11-08 12:32:45 +0100

    Some more icons

--- client/src/resources/items/index.js
+++ client/src/resources/items/index.js
@@ -3,3 +3,5 @@ export pine_cone from './pine_cone.png';
 export sickener from './sickener.png';
 export grass from './grass.png';
 export wild_strawberry from './wild_strawberry.png';
+export destroying_angel from './destroying_angel.png';
+export rumex_alpinus from './rumex_alpinus.png';
